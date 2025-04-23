02b576507dd9120334bd4ac31b718fb5a9894c02
Joseph Wong <josephwong17@gmail.com>
2025-04-23 21:14:39 +1000

    Added dropdown menus and fixed theming

--- src/app/routes.py
+++ src/app/routes.py
@@ -4,26 +4,6 @@ from flask import Blueprint, render_template
 main = Blueprint('main', __name__)
 start_time = time.time()
 
-def format_uptime(seconds):
-	units = [
-		("y", 60 * 60 * 24 * 365),
-		("d", 60 * 60 * 24),
-		("h", 60 * 60),
-		("m", 60),
-		("s", 1),
-	]
-
-	result = []
-	for name, count in units:
-		value = seconds // count
-		if value > 0 or result:
-			result.append(f"{int(value)}{name}")
-			seconds %= count
-		if len(result) == 2:
-			break
-
-	return " ".join(result) if result else "0s"
-
 @main.route("/")
 def home():
 	uptime_seconds = int(time.time() - start_time)
@@ -37,4 +17,3 @@ def server_details():
 def api_uptime():
 	uptime_seconds = int(time.time() - start_time)
 	return {"uptime_seconds": uptime_seconds}
-
