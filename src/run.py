f879e11dddb97a95712786d8320fe59e811db3f6
Joseph Wong <josephwong17@gmail.com>
2025-04-27 14:59:01 +1000

    Removed production configuration

--- src/run.py
+++ src/run.py
@@ -3,16 +3,7 @@
 import os
 from app import create_app
 
-def create_application():
-	app = create_app()
-	if os.getenv("FLASK_ENV") == "development":
-		app.config["DEBUG"] = True
-	else:
-		app.config["DEBUG"] = False
-	return app
-
-app = create_application()
+app = create_app(debug=True)
 
 if __name__ == "__main__":
-	port = int(os.getenv("PORT", 5001))
-	app.run(host="0.0.0.0", port=port)
+	app.run(port=5001)
--- src/start_dev_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PYTHONPATH=src
-export FLASK_APP=src/run.py
-export FLASK_ENV=development
-flask run --port 5001
