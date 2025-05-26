d9e3c1202c4ebf5c276c09c84c74351d769b2ddc
Joseph Wong <josephwong17@gmail.com>
2025-05-26 14:54:08 +1000

    Added database manager (also locally disallowed external connections)

---
+++ src/util/psql_manager.py
@@ -0,0 +1,119 @@
+import psycopg2
+from psycopg2 import sql
+from config_reader import ConfigReader
+
+database_config = ConfigReader.get_key_value_config("database.config")
+
+class PSQLClient:
+	def __init__(self, host=None, port=None, database=None, user=None, password=None):
+		self.host = host or database_config.get("HOST", "127.0.0.1")
+		self.port = port or database_config.get("PORT", "5432")
+		self.database = database or database_config.get("DATABASE", "postgres")
+		self.user = user or database_config.get("USER", "postgres")
+		self.password = password or database_config.get("PASSWORD", "")
+		self.conn = None
+
+	def connect(self):
+		"""Establish a connection if not already connected."""
+		if not self.conn:
+			self.conn = psycopg2.connect(
+				database=self.database,
+				user=self.user
+			)
+		return self.conn
+
+	def close(self):
+		"""Close the connection."""
+		if self.conn:
+			self.conn.close()
+			self.conn = None
+
+	def list_databases(self):
+		"""Return a list of database names."""
+		conn = self.connect()
+		with conn.cursor() as cur:
+			cur.execute(
+				"SELECT datname FROM pg_database WHERE datistemplate = false;"
+			)
+			return [row[0] for row in cur.fetchall()]
+
+	def list_tables(self, schema="public"):
+		"""Return a list of table names in the given schema."""
+		conn = self.connect()
+		with conn.cursor() as cur:
+			cur.execute(
+				sql.SQL(
+					"SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = %s;"
+				), [schema]
+			)
+			return [row[0] for row in cur.fetchall()]
+
+	def get_records(self, table, limit=100):
+		"""Fetch up to `limit` records from `table`."""
+		conn = self.connect()
+		with conn.cursor() as cur:
+			query = sql.SQL(
+				"SELECT * FROM {table} LIMIT %s;"
+			).format(
+				table=sql.Identifier(table)
+			)
+			cur.execute(query, [limit])
+			colnames = [desc[0] for desc in cur.description]
+			rows = cur.fetchall()
+			return [dict(zip(colnames, row)) for row in rows]
+
+	def execute(self, query, params=None):
+		"""Execute arbitrary SQL. Returns rows if SELECT, else commits."""
+		conn = self.connect()
+		with conn.cursor() as cur:
+			cur.execute(query, params or [])
+			if cur.description:
+				colnames = [desc[0] for desc in cur.description]
+				rows = cur.fetchall()
+				return [dict(zip(colnames, row)) for row in rows]
+			else:
+				conn.commit()
+				return None
+
+	def drop_table(self, table, schema="public"):
+		"""Drop table if it exists."""
+		conn = self.connect()
+		with conn.cursor() as cur:
+			cur.execute(
+				sql.SQL("DROP TABLE IF EXISTS {schema}.{table} CASCADE;")
+				.format(
+					schema=sql.Identifier(schema),
+					table=sql.Identifier(table)
+				)
+			)
+			conn.commit()
+
+	def create_database(self, db_name):
+		"""Create a new database."""
+		conn = self.connect()
+		with conn.cursor() as cur:
+			cur.execute(
+				sql.SQL("CREATE DATABASE {db_name};").format(
+					db_name=sql.Identifier(db_name)
+				)
+			)
+			conn.commit()
+
+	def drop_database(self, db_name):
+		"""Drop a database if it exists."""
+		conn = self.connect()
+		with conn.cursor() as cur:
+			cur.execute(
+				sql.SQL("DROP DATABASE IF EXISTS {db_name};").format(
+					db_name=sql.Identifier(db_name)
+				)
+			)
+			conn.commit()
+
+if __name__ == "__main__":
+	client = PSQLClient()
+	print("Databases:", client.list_databases())
+	print("Tables:", client.list_tables())
+	# Fetch records from a table called 'users'
+	# print(client.get_records('users', limit=10))
+	client.close()
