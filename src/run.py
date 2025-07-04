da8e5d64a3ee595335bb67392ce9606b247cc3e0
Joseph Wong <josephwong17@gmail.com>
2025-07-05 09:42:33 +1000

    Removed redundant thread starters from run.

--- src/run.py
+++ src/run.py
@@ -2,8 +2,6 @@
 
 import os
 from app import create_app
-from util.server_metrics import start_server_metrics_thread
-from util.discord_webhook import start_discord_webhook_thread
 
 app = create_app()
 
