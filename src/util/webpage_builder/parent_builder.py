7c7984929a47a52a5dd70ccceb7e98fff3251c94
Joseph Wong <josephwong17@gmail.com>
2025-09-20 19:59:39 +1000

    Added page template

--- src/util/webpage_builder/parent_builder.py
+++ src/util/webpage_builder/parent_builder.py
@@ -3,10 +3,23 @@ from util.fcr.file_config_reader import FileConfigReader
 
 class CSSManager:
     def __init__(self):
+        self.styles = []
+
+    def load_style_file(self, file_path: str):
         pass
 
 class JSManager:
     def __init__(self):
+        self.scripts = []
+
+    def load_script_file(self, file_path: str):
+        pass
+
+class ResourceManager:
+    def __init__(self):
+        self.preload_resources = []
+
+    def add_preload(self, resource: str):
         pass
 
 class WebPageBuilder:
@@ -16,9 +29,17 @@ class WebPageBuilder:
         # If privileged, we must authenticate the user.
         self.privileged = False
 
+        self.meta_title = "No Meta Title Set"
+        self.page_title = "No Title Set"
+        self.preload_resources = []
+
     @abstractmethod
     def serve_html(self): 
         """Fully compile and serve the HTML."""
+        pass
+
+    def clear_cache(self):
+        """Clear the cache for this page."""
         pass
 
     def set_cache(self, cache: dict):
